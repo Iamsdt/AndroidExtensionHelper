945176a10ad46c68d7bfe0959c62417b64b15102
Shudipto <Shudiptotrafder@gmail.com>
2019-04-29 17:32:21 +0600

    test successful

--- app/src/main/java/com/iamsdt/mylibrary/MainActivity.kt
+++ app/src/main/java/com/iamsdt/mylibrary/MainActivity.kt
@@ -2,11 +2,15 @@ package com.iamsdt.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iamsdt.androidextension.BaseActivity
+import com.iamsdt.androidextension.nextActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextActivity<MainActivity>()
     }
 }
